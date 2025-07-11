e75ee697b2affdffb0e8686695be52c82438be7e
hcooh <hcoohb@gmail.com>
2025-07-11 15:02:45 +1000

    add further options

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,29 +1,17 @@
 { config, lib, pkgs, ... }:
 
 {
-
-
-  # Perform garbage collection weekly to maintain low disk usage
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 1w";
-  };
-
-  # Optimize storage
-  # You can also manually optimize the store via:
-  #    nix-store --optimise
-  # Refer to the following link for more details:
-  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
-  nix.settings.auto-optimise-store = true;
-
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+
+    # Utilities
     git
     wget
     oh-my-zsh
@@ -32,6 +20,10 @@
     unzip
     xz
     p7zip
+
+
+    # Fonts
+    powerline-fonts
 
   ];
 
@@ -51,4 +43,20 @@
     histSize = 5000;
   };
   users.defaultUserShell = pkgs.zsh;  # set as default shell
+
+
+  # Perform garbage collection weekly to maintain low disk usage
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
+  # Optimize storage
+  # You can also manually optimize the store via:
+  #    nix-store --optimise
+  # Refer to the following link for more details:
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
+
 }
