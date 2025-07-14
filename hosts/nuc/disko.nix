9d85bbbdd459ef33542d7013035373745080ef37
hcoohb <hcoohb@gmail.com>
2025-07-14 19:28:32 +1000

    Update disko.nix

--- hosts/nuc/disko.nix
+++ hosts/nuc/disko.nix
@@ -6,7 +6,7 @@
           disk = {
             main = {
               # When using disko-install, we will overwrite this value from the commandline
-              device = "/dev/disk/by-id/some-disk-id";
+              device = "/dev/sda";
               type = "disk";
               content = {
                 type = "gpt";
