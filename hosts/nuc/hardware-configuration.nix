a5146b68fd014f4f75ee70d04de45f2b2e8c6849
hcoohb <hcoohb@gmail.com>
2025-07-14 23:30:33 +1000

    Update hardware-configuration.nix

--- hosts/nuc/hardware-configuration.nix
+++ hosts/nuc/hardware-configuration.nix
@@ -13,14 +13,14 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/74af135a-98b8-4c5f-bd44-4669472da0a7";
-      fsType = "ext4";
-    };
+  #fileSystems."/" =
+  #  { device = "/dev/disk/by-uuid/74af135a-98b8-4c5f-bd44-4669472da0a7";
+  #    fsType = "ext4";
+  #  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/ca296caf-e76d-427e-a745-95414a75a2c3"; }
-    ];
+  #swapDevices =
+  #  [ { device = "/dev/disk/by-uuid/ca296caf-e76d-427e-a745-95414a75a2c3"; }
+  #  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
